d9a36e63f9adadbec5e0545d8792247827577e2a
Jacob <jhausgaard>
2020-12-21 12:07:23 -0500

    Begun starting code

--- Attendance.java
+++ Attendance.java
@@ -1,6 +1,9 @@
 /**
- * 
+ * This contains the outlien for attendance information
  * */
 public class Attendance {
+  // This is 
+  public int daysAbs = 0;
   
+  public
 }
--- Students.java
+++ Students.java
@@ -1,7 +1,16 @@
-
 /**
- * 
+ * This contains the outline for student information
  * */
 public class Students {
+  // This is the student id that is being evalutated
+  public int studID = Interger.MIN_VALUE;
   
+  /**
+   * Constructor of a student, 
+   * @param id is the id of the student to be "found"
+   * @return returns an int with the students school id, can be used with Attendance and Grades class
+   * */
+  public int Student(int id){
+    
+  }
 }
