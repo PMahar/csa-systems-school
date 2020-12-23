397afb863628f93eb91772ac8ca7cf036f689e7a
Jacob <jhausgaard>
2020-12-23 14:30:29 -0500

    Added grading functionallity, along with better communication

--- Grades.java
+++ Grades.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
 /**
- * This will be used to get grades
+ * This will be used to get/ set and interface with grades
  * */
 public class Grades {
   
   // int[] grades will be used to pass along grade value from 
-  int[] grades = {0,0,0,0,0,0,0}; // TODO: Figure out what we want the "example"/ default grades to be
+  // grades = {Anthro grade, compsci grade, chem grade, 
+  // calc grade, quarter1, quarter2, quarter3,quarter4}
+  int[] grades = {0,0,0,0,0,0,0,0}; // TODO: Figure out what we want the "example"/ default grades to be
+  
   /**
    * This is the method to check a student's grades
    * @param id the id of the student to check grades of
    * */
   public void checkGrades(int id){
     // TODO: get the students grades from somewhere
-    System.out.print("Your student's grades are ");
-    arrayToString(grades);
+    printGrades();
   }
   
   /**
@@ -21,18 +23,86 @@ public class Grades {
    * */
   public void gradeStuds(){
     Scanner scan = new Scanner(System.in);
-    System.out.println("Do you want to view grades or change grades?");
-    if(scan.next().equalsIgnoreCase("View")){
-      
+    System.out.println("Are you grading Anthropology, Computer Science, Chemistry, or Calculus?");
+    if(scan.next().contains("nthropology")){
+      System.out.println("What would you like to change the grade to?");
+      setGrade(scan.nextInt(), 0);
+    } else if(scan.next().contains("omputer")){
+     System.out.println("What would you like to change the grade to?");
+     setGrade(scan.nextInt(), 1);
+    } else if(scan.next().contains("hemistry")){
+      System.out.println("What would you like to change the grade to?");
+      setGrade(scan.nextInt(), 2);
+    } else if(scan.next().contains("alculus")){
+      System.out.println("What would you like to change the grade to?");
+      setGrade(scan.nextInt(), 3);
+    }
+    System.out.println("WOuld you like the quarter grade to be graded for you?");
+    if(scan.next().contains("es")){
+      System.out.println("Is it the first, second, third, or fourth quarter?");
+      if(scan.next().contains("irst")){
+        
+      }
     }
   }
+  
   /**
-   * This converts an int[] to a String and then prints it
-   * @param arr the int[] to be converted
+   * Set a quarter grade based upon given information
+   * @param quarter the quarter currently in session
    * */
-  private void arrayToString(int[] arr){
-    for(int i : arr){
-      System.out.println(i + ", ");
+  private void setQuarterGrade(int quarter){
+    int anthro = grades[0];
+    int compSci = grades[1];
+    int chem = grades[2];
+    int calc = grades[3];
+    int avg = ((anthro + compSci) + (chem + calc)) / 4;
+    setGrade(avg, (quarter + 3));
+  }
+  
+  /**
+   * Prints the students grades
+   * */
+  public void printGrades(){
+    for(int i = 0; i < grades.length; i++){
+      // Switch statement to add information to the output of grades
+      switch(i){
+      case 0:
+        System.out.print("Anthropology grade is ");
+        break;
+      case 1:
+        System.out.print("Computer Science grade is ");
+        break;
+      case 2:
+        System.out.print("Chemistry grade is ");
+        break;
+      case 3:
+        System.out.print("Calculus grade is ");
+        break;
+      case 4:
+        System.out.print("Quarter 1 grade is ");
+        break;
+      case 5:
+        System.out.print("Quarter 2 grade is ");
+        break;
+      case 6:
+        System.out.print("Quarter 3 grade is ");
+        break;
+      case 7:
+        System.out.print("Quarter 4 grade is ");
+        break;
+        default:
+          break;
+      }
+      System.out.println(grades[i]);
     }
+  }
+  
+  /**
+   * Sets a value of grades[]
+   * @param grade the value to change to
+   * @param index the index of the value to change
+   * */
+  private void setGrade(int grade, int index){
+    grades[index] = grade;
   }
 }
--- Runner.java
+++ Runner.java
@@ -10,27 +10,56 @@ public class Runner{
     Scanner scan = new Scanner(System.in);
     System.out.println("Are you a teacher or parent?");
     // If the user is a teacher, prompt with appropriate teacher interfaces
-    if(scan.next().equalsIgnoreCase("teacher")){
+    if(scan.next().contains("eacher")){
       System.out.println("Are you conducting attendance, or are you dealing with grades?");
-      if(scan.next().equalsIgnoreCase("Attendance")){
+      if(scan.next().contains("ttendance")){
         // TODO: add code here to continue to attendance prompt
         attend.Attendance();
-      } else if(scan.next().equalsIgnoreCase("grades")){
+      } else /*if(scan.next().contains("rades"))*/{
         System.out.println("Do you want to change students grade or view their grades");
-        if(scan.next().contains("View")){
+        if(scan.next().contains("iew")){
           System.out.println("What is the id of the student you would like to view");
           grades.checkGrades(scan.nextInt());
-        }else if(scan.next().contains("Change")){
+        } else if(scan.next().contains("hange")){
           grades.gradeStuds();
         }
       }
-    } else if(scan.next().equalsIgnoreCase("Parent")){
+      System.out.println("Would you like to continue?");
+      if(scan.next().contains("es")){
+        System.out.println("Are you conducting attendance, or are you dealing with grades?");
+        if(scan.next().contains("ttendance")){
+          // TODO: add code here to continue to attendance prompt
+          attend.Attendance();
+        } else /*if(scan.next().contains("rades"))*/{
+          System.out.println("Do you want to change students grade or view their grades");
+          if(scan.next().contains("iew")){
+            System.out.println("What is the id of the student you would like to view");
+            grades.checkGrades(scan.nextInt());
+          } else if(scan.next().contains("hange")){
+            grades.gradeStuds();
+          }
+        }
+      } else {
+      System.exit(0);
+      }
+    } else if(scan.next().contains("arent")){
       System.out.println("Would you like to check your child's grades?"); 
-      if(scan.next().equalsIgnoreCase("Yes")){ // TODO: add case for checking report card
+      if(scan.next().contains("es")){ // TODO: add case for checking report card
         System.out.println("What is your child's student id?");
         grades.checkGrades(scan.nextInt());
       } // TODO: add code for a different scenario
       
+    }
+    System.out.println("Would you like to continue using this program?");
+    if(scan.next().contains("es")){
+        System.out.println("Would you like to check your child's grades?"); 
+      if(scan.next().contains("es")){ // TODO: add case for checking report card
+        System.out.println("What is your child's student id?");
+        grades.checkGrades(scan.nextInt());
+      } // TODO: add code for a different scenario
+      
+    } else {
+      System.exit(0); 
     }
   }
 }
