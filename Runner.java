eaf3ba2377fe2e95ece8b76150419f237cdd580a
PMahar <pmahar@techvalleyhigh.org>
2021-01-04 01:07:05 -0500

    I fixed it (kinda)

--- Runner.java
+++ Runner.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
  * */
 public class Runner{
   public static void main(String[] args){
+    //Issue - Must call main (or otherwise return to here-ish) without
+    //reconstructing objects courses, expand, grades, attend, or st
     String[] courses = new String[1];
     String[] expand = new String[courses.length + 1];
     Grades grades = new Grades();
     Attendance attend = new Attendance();
-    Student defaultStudent = new Student();
-    String[] cliArgs = args;
+    Student st = new Student();
     int studentCount = 0;
     Scanner scan = new Scanner(System.in);
     System.out.println();
@@ -29,8 +30,37 @@ public class Runner{
           } else if (scan.next().contains("hange")) {
             grades.gradeStuds();
           }
-
         case 3:
+          System.out.println("View Roster - 1 | Modify roster - 2 | Back - 3");
+          System.out.print("Please select an option: ");
+          String[] classes = new String[5];
+          int cindex = classes.length;
+          switch (scan.nextInt()) {
+            case 1:
+              st.printRoster();
+
+            case 2:
+              System.out.println("ADD STUDENT:");
+              System.out.print("Student IDs (Enter '0' to complete)");
+              //System.out.println(scan.nextLine().length());
+              while (true) {
+                int id = scan.nextInt();
+                while (id != 0) {
+                  st.newStudent(id);
+                  id = scan.nextInt();
+                }
+                break;
+              }
+              st.printRoster();
+              String[] courseList = st.addCourses();
+              main(args);
+
+            default:
+              main(args);
+          }
+
+
+/*        case 3:
           System.out.println("View Roster - 1 | Modify roster - 2 | Modify enrolled courses - 3 | Back - 4");
           System.out.print("Please select an option: ");
           switch(scan.nextInt()) {
@@ -42,7 +72,8 @@ public class Runner{
                 System.out.println();
                 System.out.println("Enrolled Courses (Enter empty line to complete)");
                 //Create a new array for every entry, duplicate previous elements
-                while (!(scan.nextLine().isEmpty())) {
+                String nxtln = scan.nextLine();
+                for (int x = 0; x < 4; x++) {
                   int items = 0;
                   //Copy between expand and students ONLY if students[items] won't be 0
                   if (items != 0) {
@@ -68,9 +99,11 @@ public class Runner{
             default:
               main(cliArgs);
           }
+          */
 
         default:
-        System.exit(0);
+          System.out.println();
+          System.exit(0);
       }
       System.out.println("Would you like to continue?");
       if (scan.next().contains("es")) {
--- Student.java
+++ Student.java
@@ -1,94 +1,60 @@
-/**
- * This contains the outline for student information
- * */
+import java.util.Scanner;
+
 public class Student {
-  // This is the student id that is being evalutated
-  private int studID = Integer.MIN_VALUE;
-  private int classIndex;
-  private int[] indices;
-  private String[] students;
-  private String[] studClasses;
-
-  /**
-  *Accessor for studID
-  *@return Student ID as integer
-  */
-
-  public int getStudentID(){
-    return this.studID;
-  }
+  private int sID;
+  private String[] sClasses;
+  int studentCount = 0;
+  String[] students = new String[studentCount];
+  String[] studBackup;
+  String[] classBackup;
 
   public Student() {
 
   }
 
-  /**
-  *Constructor Student instantiates a new Student object with ID and classes
-  *@param sID Student id as integer
-  *@param sClasses Enrollment list for student in string array
-  */
-  public Student(int sID, int cIndex, String[] sClasses) {
-    this.studID = sID;
-    this.classIndex = cIndex;
-    this.indices = new int[0];
-    this.studClasses = sClasses;
-    for (int i = 0; i < sClasses.length; i++) {
-      System.out.println(sClasses[i]);
-    }
-    //Expand the current array of students by one per instantiation of Student constructor
-    //Copy the contents of students to expand, then reconstruct students with previous elements plus new id
-    if (students != null) {
-      String[] expand = new String[students.length + 1];
+  public void newStudent(int id) {
+    this.students = new String[studentCount + 1];
+    if (!(studBackup == null)) {
       for (int i = 0; i < students.length; i++) {
-        expand[i] = students[i];
-        students = new String[expand.length];
-        for (int j = 0; j < students.length; j++) {
-          students[j] = expand[j];
-        }
-        students[students.length - 2] = "" + sID;
+        students[i] = studBackup[i];
       }
-    } else {
-      students = new String[1];
-      students[0] = "" + sID;
     }
-    int[] expand;
-//    if (indices == null) {
-      indices = new int[students.length + 1];
-//    }
-    expand = new int[indices.length + 1];
-    System.out.println("students" + students.length);
-    System.out.println("indices" + indices.length);
-    System.out.println("expand" + expand.length);
+    studentCount++;
+    students[studentCount - 1] = "" + id;
+    this.studBackup = new String[students.length + 1];
     for (int i = 0; i < students.length; i++) {
-      //Make a list of indices to assign IDs to course lists
-      for (int j = 0; j < expand.length - 1; j++) {
-        indices[j] = expand[j];
-        indices[students.length - 1] = classIndex;
-      }
-      expand[i] = indices[i];
+      studBackup[i] = students[i];
     }
+//debug:
+//    for (int x = 0; x < backup.length; x++) {
+//      System.out.println("BACKUP: " + backup[x]);
+//    }
+//    for (int x = 0; x < students.length; x++) {
+//      System.out.println("STUDENTS: " + students[x]);
+//    }
   }
 
-  public void viewRoster() {
+  public String[] addCourses() {
+    Scanner cs = new Scanner(System.in);
+    String course = "";
+    String[] enrollments = new String[students.length];
     for (int i = 0; i < students.length; i++) {
-      String classes = "";
-      String prevClasses = "";
-      for (int j = 0; j < studClasses.length; j++) {
-        classes = prevClasses + studClasses[j];
-        prevClasses = classes;
+      System.out.println("ID " + students[i] + "- Number of enlisted courses : ");
+      int enlist = cs.nextInt();
+      for (int j = 0; j < enlist; j++) {
+        System.out.print("Course " + (j + 1) + " for student " + students[i] + ": ");
+        course += cs.nextLine() + ", ";
       }
-      System.out.println("[" + i + 1 + "]     " + "[" + students[i] + "]    " + classes);
+      enrollments[i] = course;
+      course = null;
     }
+    return enrollments;
   }
 
-
-//Test main for debugging
-//  public static void main(String[] args) {
-//   String[] classes = new String[]{"Anthro", "CSA", "Calc AB"};
-//    Student st = new Student(2396, classes);
-//    System.out.println(st.studID);
-//    for (int i = 0; i < st.studClasses.length; i++) {
-//      System.out.println(st.studClasses[i]);
-//    }
-//  }
+  public void printRoster() {
+    System.out.println();
+    for (int i = 0; i < students.length; i++) {
+      System.out.println("[" + students[i] + "]");
+    }
+  }
 }
