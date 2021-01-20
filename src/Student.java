aafb149fb363d85ffbe7dbde73835cac95f306a7
PMahar <pmahar@techvalleyhigh.org>
2021-01-20 00:34:16 -0500

    Convert Student from string[] to object type

---
+++ src/Student.java
@@ -0,0 +1,15 @@
+public class Student {
+  //Every Student object will require an individual name, ID, and list of enrolled courses
+  //(as well as any other objects like lunch requirement)
+  int studentID;
+  String studentName;
+
+  //Add an element to the student array
+
+  //Constructor to instantiate student object from psvm
+  public Student(String name, int id) {
+    this.studentName = name;
+    this.studentID = id;
+  }
+
+}
