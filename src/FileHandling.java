ccc030b43d0da7a407724dabc4d8bbb31ce4acaa
Jacob <jhausgaard>
2021-04-13 14:39:38 -0400

    Forgot to add the HashMap import

--- src/FileHandling.java
+++ src/FileHandling.java
@@ -3,6 +3,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
